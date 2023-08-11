5f5002091e5a364231973e539ed0e8cc88e8ac7b
marketoshaIG <137831388+marketoshaIG@users.noreply.github.com>
2023-08-10 20:12:57 -0500

    commit 1

--- part3/studio/src/components/BoardAssignment.js
+++ part3/studio/src/components/BoardAssignment.js
@@ -1,15 +1,38 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
-
+   const boards = [
+      {
+       "label" : "Breakfast", 
+       "value": "1"
+       },
+      {
+       "label": "Lunch",
+       "value":"2"
+       },
+      {
+       "label":"Dinner" ,
+       "value": "3"
+      }
+   ];
+    
+   const [boardName, setName] = useState("no boards yet!")
    const handleChange = (event) => {
+      setName(event.target.value);
    }
+   
+   const boardOptions = boards.map(board => {
+      return (
+                  <option value={board.value}>{board.label}</option>
+             ); 
+   });
+     
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         {boardOptions}
       </select>
 
       <p>Saved to {boardName}!</p>
--- part3/studio/src/components/Recipe.js
+++ part3/studio/src/components/Recipe.js
@@ -1,62 +1,84 @@
-const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+import React from 'react';
 
+const RecipeAuthor = ({ authorName, authorPhoto, authorLink }) => {
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt="" style={{ objectFit: "contain", borderRadius: "50%" }} />
          <div>
             <h3>{authorName}</h3>
-            <a href={authorLink}></a> 
+            <a href={authorLink}>Author's Instagram</a>
          </div>
       </div>
    );
 }
 
-const RecipeIngredients = () => {
-   const ingredients = [];
-   return(
+const RecipeIngredients = ({ ingredients }) => {
+   return (
       <div>
          <h3>Recipe Ingredients</h3>
          <ul>
-            <li>{ingredients[0]}</li>
-            <li>{ingredients[1]}</li>
-            <li>{ingredients[2]}</li>
-            <li>{ingredients[3]}</li>
-            <li>{ingredients[4]}</li>
+            {ingredients.map((ingredient, index) => (
+               <li key={index}>{ingredient}</li>
+            ))}
          </ul>
       </div>
    );
 }
 
-const RecipeDescription = () => {
+const RecipeDescription = ({ recipeName, description, ingredients, authorName, authorPhoto, authorLink }) => {
    return (
-      <div> 
+      <div>
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>{recipeName}</h1>
+            <p>{description}</p>
          </div>
          <div className="recipePhotoBlock">
-            <RecipeIngredients />
-            <RecipeAuthor />
+            <RecipeIngredients ingredients={ingredients} />
+            <RecipeAuthor
+               authorName={authorName}
+               authorPhoto={authorPhoto}
+               authorLink={authorLink}
+            />
          </div>
       </div>
    );
 }
 
-const RecipePhoto = () => {
+const RecipePhoto = ({ recipeImage }) => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src={recipeImage} alt="avocado" className="imageUpdates" />
    );
 }
 
-export default function RecipeDisplay () {
-   return(
+export default function RecipeDisplay() {
+   const recipeInfo = {
+      recipeName: "Avocado toast",
+      authorName: "Iryna Ignatenko",
+      description: "Quick and easy Avocado Toast with Egg 4 ways – fried, scrambled, boiled and poached. It’s a simple healthy protein-packed breakfast, snack or light meal! Simple to make, tasty and satisfying. If you haven’t had an egg and avocado toast breakfast yet, now is the time!",
+      authorLink: "https://www.instagram.com/mybreakfast_idea/",
+      authorPhoto: "https://thesaltypot.com/wp-content/uploads/2019/02/Avocado-Toast09.jpg",
+      recipeImage: "https://thesaltypot.com/wp-content/uploads/2019/02/Avocado-Toast09.jpg",
+      ingredients: [
+         "Avocado",
+         "Bread",
+         "Salt",
+         "Lemon",
+         "Eggs"
+      ]
+   };
+
+   return (
       <div className="recipePhotoBlock">
-         <RecipePhoto />
-         <div>
-            <RecipeDescription />
+         <RecipePhoto recipeImage={recipeInfo.recipeImage} />
+         <div >
+            <RecipeDescription 
+               recipeName={recipeInfo.recipeName}
+               description={recipeInfo.description}
+               ingredients={recipeInfo.ingredients}
+               authorName={recipeInfo.authorName}
+               authorPhoto={recipeInfo.authorPhoto}
+               authorLink={recipeInfo.authorLink}
+            />
          </div>
       </div>
    );
